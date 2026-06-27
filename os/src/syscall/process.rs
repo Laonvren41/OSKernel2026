78a80eade4e17d1a0f32b3e2e1d2d4b03585f81f
外卖只点拼好饭 <team@example.com>
2026-06-28 05:09:36 +0800

    修复file()返回Result不是Option

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -388,12 +388,14 @@ pub fn sys_exec(pathp: *const u8, mut args: *const usize, mut envp: *const usize
         abs_path = String::from("/musl/busybox");
     }
     debug!("to open,the path is: {}", abs_path);
-    let app_inode = match open(&abs_path, OpenFlags::O_RDONLY, NONE_MODE, cwd) {
-        Ok(f) => match f.file() {
-            Some(ff) => ff,
-            None => return -1,
-        },
-        Err(_) => return -1,
+    let app_inode = if let Ok(f) = open(&abs_path, OpenFlags::O_RDONLY, NONE_MODE, cwd) {
+        if let Ok(ff) = f.file() {
+            ff
+        } else {
+            return -1;
+        }
+    } else {
+        return -1;
     };
     inner.fs_info.set_exe(abs_path);
     let elf_data = app_inode.inode.read_all().unwrap();
